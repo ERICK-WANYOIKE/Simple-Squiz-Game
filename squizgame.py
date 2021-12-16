ff306f349e6aa81ebbcd58e69e96684bc5846d78
Apple <apple@Apples-MacBook-Pro-526.local>
2021-12-17 01:00:25 +0400

    first commit

---
+++ squizgame.py
@@ -0,0 +1,30 @@
+#input()
+#print()
+
+print("welcome to my computer game")
+playing = raw_input("Do you want to play: ")
+
+if playing != "yes" :
+    quit()
+
+print("okay lets play! ")
+
+answers = raw_input("what does CPU stands for? ")
+if answers == "central processing unit" :
+    print(" answer is correct!")
+else:
+        print("answer is wrong !")
+
+answers = raw_input("what does GPU stands for? ")
+if answers == "graphics processing unit" :
+    print(" answer is correct!")
+else:
+        print("answer is wrong !")
+    
+answers = raw_input("what does RAM stands for? ")
+if answers == " random access memory" :
+    print(" answer is correct!")
+else:
+        print("answer is wrong !")
+
+    
